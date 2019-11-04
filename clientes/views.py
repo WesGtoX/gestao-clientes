1629ee80c6ae7af2e1b7acf1f27b23558a3229ef
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-11-03 23:31:36 -0300

    Models inside the terminal, advanced database searches and sorting queries

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-
-from .models import Person
+from .models import Person, Produto
 from .forms import PersonForm
+from django.views.generic import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -90,3 +91,16 @@ class PersonDelete(DeleteView):
 
     def get_success_url(self):
         return reverse_lazy('person_list_cbv')
+
+
+# Bulk create
+class ProdutoBulk(View):
+
+    def get(self, request):
+        produtos = ['Banana', 'Maca', 'Limao', 'Laranja', 'Pera', 'Melancia']
+        list_produtos = []
+        for produto in produtos:
+            p = Produto(descricao=produto, preco=10)
+            list_produtos.append(p)
+        Produto.objects.bulk_create(list_produtos)
+        return HttpResponse('Funcionou')
